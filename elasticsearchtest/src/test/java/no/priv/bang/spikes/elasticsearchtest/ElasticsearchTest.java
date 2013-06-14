ea110a08b6304074290dc5ccb68f6d842d01e51c
Steinar Bang <sb@dod.no>
2013-06-14 21:10:18 +0200

    Initial commit.

---
+++ elasticsearchtest/src/test/java/no/priv/bang/spikes/elasticsearchtest/ElasticsearchTest.java
@@ -0,0 +1,14 @@
+package no.priv.bang.spikes.elasticsearchtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ElasticsearchTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
