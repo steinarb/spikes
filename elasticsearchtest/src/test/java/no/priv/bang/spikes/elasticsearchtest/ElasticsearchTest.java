704609dab459a237fe8a20f4b745400ca83c3102
Steinar Bang <sb@dod.no>
2013-06-14 22:16:49 +0200

    Start and stop a node in the unit test.

--- elasticsearchtest/src/test/java/no/priv/bang/spikes/elasticsearchtest/ElasticsearchTest.java
+++ elasticsearchtest/src/test/java/no/priv/bang/spikes/elasticsearchtest/ElasticsearchTest.java
@@ -2,13 +2,21 @@ package no.priv.bang.spikes.elasticsearchtest;
 
 import static org.junit.Assert.*;
 
+import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
 import org.junit.Test;
+import static org.elasticsearch.node.NodeBuilder.*;
 
 public class ElasticsearchTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// on startup
+		Node node = nodeBuilder().node();
+		Client client = node.client();
+
+		// on shutdown
+		node.close();	
 	}
 
 }
