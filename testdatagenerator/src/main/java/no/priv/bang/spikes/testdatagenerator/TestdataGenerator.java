f18dc03bab7b5e21be99c98b1772129fbcf8c14e
Steinar Bang <sb@dod.no>
2013-06-17 12:16:23 +0200

    Initial commit.

---
+++ testdatagenerator/src/main/java/no/priv/bang/spikes/testdatagenerator/TestdataGenerator.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package no.priv.bang.spikes.testdatagenerator;
+
+/**
+ * @author sbang
+ * 
+ * Main entry point for a command line utility to generate test data to
+ * pump into elasticsearch.
+ *
+ */
+public class TestdataGenerator {
+
+	/**
+	 * @param args the arguments given on the command line
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello elasticsearch world!");
+	}
+
+}
